5060d195f217e31d02a947026093430960a10240
wdw110 <wudw110@gmail.com>
2016-11-23 22:03:58 +0800

    add some lines

--- hello.c
+++ hello.c
@@ -13,6 +13,25 @@ int main() {
 	printf("精度值：%d\n", FLT_DIG);
 	printf("Hello\tWorld\n\n");
 
+	int a = 21;
+	int b = 10;
+	int c;
+
+	c = a + b;
+	printf("Line 1 - c的值是 %d\n", c);
+	c = a - b;
+	printf("Line 2 - c的值是 %d\n", c);
+	c = a / b;
+	printf("Line 3 - c的值是 %d\n", c);
+	c = a % b;
+	printf("Line 4 - c的值是 %d\n", c);
+	c = a * b;
+	printf("Line 5 - c的值是 %d\n", c);
+	c = a++;
+	printf("Line 6 - c的值是 %d\n", c);
+	c = a--;
+	printf("Line 7 - c的值是 %d\n", c);
+
 	return 0;
 }
 
