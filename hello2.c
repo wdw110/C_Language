3568e7c0f89b594c3af237360d9ca987a904a081
wdw110 <wudw110@gmail.com>
2016-11-24 22:49:55 +0800

    add hello2.c script

---
+++ hello2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* 函数声明 */
+void func(void);
+
+static int count = 5; /* 全局变量 */
+
+int main()
+{
+	while (count--)
+	{
+		func();
+	}
+	return 0;
+}
+
+/* 函数定义 */
+void func(void)
+{
+	static int i = 5; /* 局部静态变量 */
+	i++;
+
+	printf("i is %d and count is %d\n", i, count);
+}
+
+
+
+
+
+
+
